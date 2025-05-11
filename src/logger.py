d398cc91d5eccabe4289deb7e077ae234ae706d2
Keerthan Kini <keerthank7747@gmail.com>
2025-05-11 11:51:18 +0530

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+   
+import logging
+import os
+from datetime import datetime
+
+# Create a logs folder in the current directory
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Create a timestamped log filename
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
